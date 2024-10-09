7e3cf2d45bc356e5956557eeee6cea4ed7232266
armaxri <armaxri@gmail.com>
2024-10-10 00:55:39 +0200

    Changed handling of compile_commands.json file to allow handling of windows files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use std::{fs::File, io::BufReader, path::PathBuf, time::Instant};
 
-use ast_reader::clang_ast_parser::{ClangAstParser, ClangAstParserImpl};
+use ast_reader::{
+    clang_ast_parser::{ClangAstParser, ClangAstParserImpl},
+    compile_commands_reader::read_compile_commands_json_file,
+};
 use process::{clang_compile2ast_call::clang_compile2ast_call, terminal_process::TerminalProcess};
 
 pub mod ast_reader;
@@ -10,9 +13,12 @@ pub mod process;
 pub fn dry_run_ast_parser(compile_commands_json: &PathBuf) {
     let start_time_all = Instant::now();
 
-    let file = File::open(compile_commands_json).expect("Failed to open file");
-    let reader = BufReader::new(file);
-    let entries: Vec<_> = json_compilation_db::read(reader).collect();
+    let entries = if read_compile_commands_json_file(compile_commands_json).is_some() {
+        read_compile_commands_json_file(compile_commands_json).unwrap()
+    } else {
+        println!("Error reading compile_commands.json file");
+        return;
+    };
 
     println!(
         "Found {} entries in file {}",
@@ -21,29 +27,21 @@ pub fn dry_run_ast_parser(compile_commands_json: &PathBuf) {
     );
 
     for entry in entries {
-        match entry {
-            Ok(entry) => {
-                let start_time = Instant::now();
+        let start_time = Instant::now();
 
-                let terminal_process = Box::new(TerminalProcess::new(clang_compile2ast_call(
-                    &entry.arguments.join(" "),
-                )));
-                let mut parser: ClangAstParserImpl = ClangAstParserImpl::new(terminal_process);
-                let ast = parser.parse_ast();
+        let terminal_process =
+            Box::new(TerminalProcess::new(clang_compile2ast_call(&entry.command)));
+        let mut parser: ClangAstParserImpl = ClangAstParserImpl::new(terminal_process);
+        let ast = parser.parse_ast();
 
-                let ast_element_count = if ast.is_some() { ast.unwrap().len() } else { 0 };
+        let ast_element_count = if ast.is_some() { ast.unwrap().len() } else { 0 };
 
-                println!(
-                    "Read {} AST Elements in {:?} in File: {}",
-                    ast_element_count,
-                    start_time.elapsed(),
-                    entry.file.display()
-                );
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
+        println!(
+            "Read {} AST Elements in {:?} in File: {}",
+            ast_element_count,
+            start_time.elapsed(),
+            entry.file
+        );
     }
 
     let elapsed_all = start_time_all.elapsed();
