2d2b77762d2c47d5d1a501d9a04898789f7a0415
armaxri <armaxri@gmail.com>
2024-10-10 02:04:39 +0200

    Added integration test for processor (actually I want ast files).

--- tests/playground_dryrun.rs
+++ tests/playground_dryrun.rs
@@ -1,12 +1,50 @@
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
+    use std::{
+        fs::File,
+        io::{LineWriter, Write},
+        path::PathBuf,
+    };
 
-    use rust_clang_call_graph::dry_run_ast_parser;
+    use rust_clang_call_graph::{
+        ast_reader::compile_commands_reader::read_compile_commands_json_file,
+        dry_run_ast_parser,
+        process::{
+            clang_compile2ast_call::clang_compile2ast_call, terminal_process::TerminalProcess,
+            Process,
+        },
+    };
 
     #[test]
     fn dry_run_ast_parser_test() {
         let compile_commands_json = PathBuf::from("tests/playground/compile_commands.json");
         dry_run_ast_parser(&compile_commands_json);
+    }
+
+    #[test]
+    fn process_files2ast_files() {
+        let compile_commands_json = PathBuf::from("tests/playground/compile_commands.json");
+
+        let entries = if read_compile_commands_json_file(&compile_commands_json).is_some() {
+            read_compile_commands_json_file(&compile_commands_json).unwrap()
+        } else {
+            assert!(false);
+            return;
+        };
+
+        for entry in entries {
+            let mut terminal_process = TerminalProcess::new(clang_compile2ast_call(&entry.command));
+
+            assert!(terminal_process.process());
+
+            let file =
+                File::create(entry.file[..entry.file.len() - 3].to_string() + "ast").unwrap();
+            let mut writer = LineWriter::new(file);
+            while terminal_process.has_next_line() {
+                let buffer = terminal_process.get_next_line();
+                writer.write(buffer.as_bytes()).unwrap();
+                writer.write("\n".as_bytes()).unwrap();
+            }
+        }
     }
 }
