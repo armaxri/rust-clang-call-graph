23fb2fd1305333b7a96131b1126b6563c7c39340
armaxri <armaxri@gmail.com>
2024-10-10 21:46:24 +0200

    Transferred location functions.

--- src/call_graph/data_structure/location.rs
+++ src/call_graph/data_structure/location.rs
@@ -6,3 +6,51 @@ pub struct Location {
     pub line: usize,
     pub column: usize,
 }
+
+impl Location {
+    pub fn new(line: usize, column: usize) -> Self {
+        Location { line, column }
+    }
+
+    pub fn is_before(&self, other: &Location) -> bool {
+        if self.line < other.line {
+            return true;
+        } else if self.line == other.line {
+            return self.column < other.column;
+        }
+        false
+    }
+
+    pub fn is_location_same_or_after(&self, other: &Location) -> bool {
+        return !self.is_before(other);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_before() {
+        let location = Location::new(1, 1);
+        let mut other = Location::new(2, 2);
+        assert!(location.is_before(&other));
+        other = Location::new(1, 1);
+        assert!(!location.is_before(&other));
+        other = Location::new(0, 0);
+        assert!(!location.is_before(&other));
+    }
+
+    #[test]
+    fn test_is_location_same_or_after() {
+        let location = Location::new(1, 1);
+        let mut other = Location::new(0, 0);
+        assert!(location.is_location_same_or_after(&other));
+        other = Location::new(1, 0);
+        assert!(location.is_location_same_or_after(&other));
+        other = Location::new(1, 1);
+        assert!(location.is_location_same_or_after(&other));
+        other = Location::new(2, 2);
+        assert!(!location.is_location_same_or_after(&other));
+    }
+}
