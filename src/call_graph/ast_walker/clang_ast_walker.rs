7fd39a00a316396087743da550298ca53813f719
armaxri <armaxri@gmail.com>
2024-10-30 01:12:25 +0100

    Started the walker and implemented the bases of the test concept.

--- src/call_graph/ast_walker/clang_ast_walker.rs
+++ src/call_graph/ast_walker/clang_ast_walker.rs
@@ -2,21 +2,49 @@ use std::{cell::RefCell, collections::VecDeque, rc::Rc};
 
 use crate::{
     ast_reader::clang_ast_element::ClangAstElement,
-    call_graph::database::database_sqlite::DatabaseSqlite,
+    call_graph::{
+        data_structure::{file_structure::FileStructure, MainDeclPosition},
+        database::database_sqlite::DatabaseSqlite,
+    },
 };
 
 struct ClangAstWalkerInternal {
     pub db: Rc<RefCell<DatabaseSqlite>>,
+    pub file_path: String,
+    pub current_file: Rc<RefCell<FileStructure>>,
 }
 
 pub fn walk_ast_2_func_call_db(
-    _file_path: &str,
-    _parsed_ast: VecDeque<ClangAstElement>,
-    _db: Rc<RefCell<DatabaseSqlite>>,
+    file_path: &str,
+    parsed_ast: VecDeque<ClangAstElement>,
+    db: Rc<RefCell<DatabaseSqlite>>,
 ) {
-    let walker = ClangAstWalkerInternal { db: _db };
+    // Make sure that the file is in the database, so that we can reference it.
+    let main_file = db.borrow().get_or_add_cpp_file(&file_path);
+    let mut current_file_name_str = main_file.borrow().get_name().to_string();
 
-    for _ast_element in _parsed_ast {
-        // Do something with _ast_element
+    let mut walker = ClangAstWalkerInternal {
+        db: db,
+        file_path: file_path.to_string(),
+        current_file: main_file.clone(),
+    };
+
+    for ast_element in parsed_ast {
+        if *ast_element.file == "" {
+            continue;
+        }
+
+        if *ast_element.file != current_file_name_str {
+            if *ast_element.file == walker.file_path {
+                walker.current_file = main_file.clone();
+            } else {
+                walker.current_file = walker.db.borrow().get_or_add_hpp_file(&ast_element.file);
+                walker
+                    .current_file
+                    .borrow_mut()
+                    .add_referenced_from_source_file(&main_file);
+            }
+            current_file_name_str = walker.current_file.borrow().get_name().to_string();
+        }
     }
 }
---
+++ tests/playground_parser_and_walker_run.rs
@@ -0,0 +1,251 @@
+#[cfg(test)]
+mod tests {
+    use std::{cell::RefCell, path::PathBuf, rc::Rc};
+
+    use rust_clang_call_graph::{
+        ast_reader::clang_ast_parser::{ClangAstParser, ClangAstParserImpl},
+        call_graph::{
+            ast_walker::clang_ast_walker::walk_ast_2_func_call_db,
+            database::{database_content::DatabaseContent, database_sqlite::DatabaseSqlite},
+        },
+        process::dummy_process::DummyProcess,
+    };
+
+    fn run_ast_parser_test(test_dir_path: &str, ast2load_file_names: Vec<&str>) {
+        let test_dir = PathBuf::from(test_dir_path);
+        let db_path = test_dir.join("created_db.db");
+        let database_sqlite = Rc::new(RefCell::new(DatabaseSqlite::create_database(
+            &db_path, true,
+        )));
+
+        for ast2load_file_name in ast2load_file_names {
+            let ast2load_file = test_dir.join([ast2load_file_name, ".ast2load"].join(""));
+            let cpp_file = test_dir.join([ast2load_file_name, ".cpp"].join(""));
+            let dummy_process = Box::new(DummyProcess::new_from_file(
+                &ast2load_file.to_str().unwrap().to_string(),
+            ));
+
+            let mut parser = ClangAstParserImpl::new(dummy_process);
+            let ast = parser.parse_ast();
+
+            match ast {
+                Some(ast) => {
+                    walk_ast_2_func_call_db(
+                        &cpp_file.to_str().unwrap(),
+                        ast,
+                        database_sqlite.clone(),
+                    );
+                }
+                None => {
+                    assert!(false);
+                }
+            }
+        }
+
+        let sqlite_content = database_sqlite.borrow().get_db_content();
+        let db_json_file = test_dir.join("created_db.json");
+        sqlite_content.save_to_file(&db_json_file.to_str().unwrap());
+
+        let expected_db_content_json_file_name = test_dir.join("expected_db_content.json");
+        let expected_db_content =
+            DatabaseContent::load_from_file(&expected_db_content_json_file_name.to_str().unwrap());
+
+        assert_eq!(sqlite_content, expected_db_content);
+    }
+
+    #[cfg(test)]
+    mod complex_cases {
+        use super::*;
+
+        #[test]
+        #[ignore]
+        fn simple_gtest_test() {
+            run_ast_parser_test("./tests/playground/complexCases/simpleGtest", vec!["file"]);
+        }
+    }
+
+    #[cfg(test)]
+    mod c_style_tests {
+        use super::*;
+
+        #[test]
+        #[ignore]
+        fn decl_in_header_and_2_cpps_test() {
+            run_ast_parser_test(
+                "./tests/playground/cStyleTests/funcCallInFuncCall",
+                vec!["impl", "main"],
+            );
+        }
+
+        #[test]
+        fn func_call_in_func_call_test() {
+            run_ast_parser_test(
+                "./tests/playground/cStyleTests/funcCallInFuncCall",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn funcs_test() {
+            run_ast_parser_test("./tests/playground/cStyleTests/funcs", vec!["file"]);
+        }
+
+        #[test]
+        #[ignore]
+        fn funcs_with_headers_test() {
+            run_ast_parser_test(
+                "./tests/playground/cStyleTests/funcsWithHeaders",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn multiline_func_call_in_func_call_test() {
+            run_ast_parser_test(
+                "./tests/playground/cStyleTests/multilineFuncCallInFuncCall",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn printf_test() {
+            run_ast_parser_test("./tests/playground/cStyleTests/printf", vec!["file"]);
+        }
+
+        #[test]
+        #[ignore]
+        fn simple_decl_in_header_impl_in_header_and_one_cpp_test() {
+            run_ast_parser_test(
+                "./tests/playground/cStyleTests/simpleDeclInHeaderImplInHeaderAndOneCpp",
+                vec!["main"],
+            );
+        }
+    }
+
+    #[cfg(test)]
+    mod simple_cpp_classes {
+        use super::*;
+
+        #[test]
+        #[ignore]
+        fn class_call_from_lambda_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleCppClasses/classCallFromLambda",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn class_in_class_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleCppClasses/classInClass",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn class_raw_pointer_call_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleCppClasses/classRawPointerCall",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn class_unique_pointer_call_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleCppClasses/classUniquePointerCall",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn inherited_virtual_method_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleCppClasses/inheritedVirtualMethod",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn method_test() {
+            run_ast_parser_test("./tests/playground/simpleCppClasses/method", vec!["file"]);
+        }
+
+        #[test]
+        #[ignore]
+        fn static_method_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleCppClasses/staticMethod",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn struct_method_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleCppClasses/structMethod",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn virtual_method_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleCppClasses/virtualMethod",
+                vec!["file"],
+            );
+        }
+    }
+
+    #[cfg(test)]
+    mod simple_templates {
+        use super::*;
+
+        #[test]
+        #[ignore]
+        fn simple_template_class_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleTemplates/simpleTemplateClass",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn simple_template_class_virtual_func_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleTemplates/simpleTemplateClassVirtualFunc",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn simple_template_function_with_class_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleTemplates/simpleTemplateFunctionWithClass",
+                vec!["file"],
+            );
+        }
+
+        #[test]
+        #[ignore]
+        fn simple_template_with_two_classes_test() {
+            run_ast_parser_test(
+                "./tests/playground/simpleTemplates/simpleTemplateWithTwoClasses",
+                vec!["file"],
+            );
+        }
+    }
+}
